5fe55db8f45835c46bac7547170517f854f552e9
JP <jp@jp.nl>
2023-01-23 10:51:43 +0100

    Eerste demo unittests laten zien

---
+++ demo/DemoSolution/DemoProject.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace DemoProject.Tests
+{
+	public class UnitTest1
+	{
+
+		[Fact]
+		public void AddTest()
+		{
+			// Arrange
+			var sut = new Calculator(); // system under test
+
+			// Act
+			sut.Add(4);
+			sut.Add(8);
+			sut.Add(15);
+			sut.Add(16);
+			sut.Add(23);
+			sut.Add(42);
+
+			// Assert
+			Assert.Equal(108, sut.Result);
+		}
+
+		[Fact]
+		public void Test1()
+		{
+			Assert.Equal(4, 4);
+			Assert.Equal("hoi", "hoi");
+			Assert.NotEqual("hoi", "doei");
+		}
+	}
+}
---
+++ demo/DemoSolution/DemoProject/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoProject
+{
+	public class Calculator
+	{
+
+		public int Result { get; set; }
+
+		public void Add(int x)
+		{
+			Result += x;
+		}
+	}
+}
